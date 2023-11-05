27fed037eb5751ea7708d7504623d2884c6ebacd
Wagner Bastos <wagnerbastos1987@gmail.com>
2023-11-04 21:23:28 -0400

    add some lines to make program more believable

--- TransferToWallets.py
+++ TransferToWallets.py
@@ -6,29 +6,33 @@ locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
 print("Starting program...")
 sleep(3)
-print("\nDistribute the Dark Army's funds among all individuals with bank accounts in both NYC and Scranton.")
+print("\nDistribute the Dark Army's funds among all individuals with bank accounts in NYC.")
 sleep(2)
 
 name = input("Enter your name: ")
 
 while True:
     bank_account = input("Do you possess a bank account? Please type 'Y' for yes or 'N' for no. ").lower()
-    sleep(2)
+    sleep(2)    
     if bank_account == 'y':
-        print("You are eligible to claim a portion of the Dark Army's funds.")
+        print("\nThe system will conduct a search for a bank account registered under the previously provided name, with a verified address in the city of New York.")
         sleep(2)
-        print("You will be awarded a random amount ranging from $0 to $12,000,000.00.")
+        print("\nAccount located. Transfer process will commence shortly.")
+        sleep(2)
+        print("\nYou are eligible to claim a portion of the Dark Army's funds.")
+        sleep(2)
+        print("\nYou will be awarded a random amount ranging from $0 to $24,000,000.00.")
         sleep(2)
         print("\nStarting transference...")
         sleep(2)
         count = 0
         while count <= 10:
             print("Transferring...")
-            count += 1            
+            count += 1
             sleep(1)
         print("Transference has been successfully completed!")        
         
-        random_value = random.uniform(1, 12_000_000)
+        random_value = random.uniform(1, 24_000_000)
         formatted_value = locale.format_string('%d', random_value, grouping=True)
         decimal_part = "%.2f" % (random_value % 1)
         total_value = formatted_value + decimal_part[1:]
