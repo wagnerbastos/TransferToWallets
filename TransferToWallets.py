7d7090d92696223a943af8467dd29a70f8852c46
Wagner Bastos <wagnerbastos1987@gmail.com>
2023-11-09 16:44:01 -0500

    add time period for the program to be available; add option to enter ssn; improve the general looks of the program

--- TransferToWallets.py
+++ TransferToWallets.py
@@ -1,24 +1,52 @@
+import datetime
 import locale
 import random
 from time import sleep
 
+current_datetime = datetime.datetime.now()
+formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
 print("Starting program...")
 sleep(3)
 print("\nDistribute the Dark Army's funds among all individuals with bank accounts in NYC.")
 sleep(2)
+print(f"The distribution will be available for the next 15 minutes, starting {formatted_datetime}.")
+sleep(2)
 
 name = input("Enter your name: ")
+print(f"The name entered by you was {name.title()}.")
+
+valid_ssn = None
+while valid_ssn is None:
+    ssn = input("Enter your Social Security Number (9 digits, numbers only): ")
+    ssn = ssn.replace(" ", "").replace("-", "")
+    if len(ssn) == 9 and ssn.isdigit():
+        valid_ssn = ssn
+    else:
+        print("Invalid SSN. SSN should have nine digits. Please try again.")
+        
+    if valid_ssn is not None:
+        print(f"You entered a valid SSN: {valid_ssn}.")
 
 while True:
     bank_account = input("Do you possess a bank account? Please type 'Y' for yes or 'N' for no. ").lower()
     sleep(2)    
     if bank_account == 'y':
-        print("\nThe system will conduct a search for a bank account registered under the previously provided name, with a verified address in the city of New York.")
+        print("\nThe system will conduct a search for a bank account registered under the previously provided name and SSN, with a verified address in the city of New York.")
+        sleep(3)
+        print("\nPlease note that this process may take several minutes. If you initiated the process within the specified time frame, you will still receive your portion even if the verification is completed after the designated period.\n")
+        sleep(3)
+        
+        count = 0
+        while count <= 15:
+            print("Locating account...")
+            count += 1
+            sleep (2)
+            
+        print("\nAccount located.")            
         sleep(2)
-        print("\nAccount located.")
-        sleep(2)
+        
         print("\nYou are eligible to claim a portion of the Dark Army's funds.")
         sleep(2)
         print("\nTransfer process will commence shortly.")
@@ -27,6 +55,7 @@ while True:
         sleep(2)
         print("\nStarting transference...")
         sleep(2)
+        
         count = 0
         while count <= 10:
             print("Transferring...")
@@ -50,5 +79,5 @@ while True:
         print("Sorry, that's not an accepted answer. Please try again.")
 
 print("DISCLAIMER:\n")
-print("This is a work of fiction. Any names or characters, businesses or places, events or incidents, are fictitious.\nAny resemblance to actual persons, living or dead, or actual events, is purely coincidental.")
-print("\nNo animals were harmed during the making of this program.")
+print("This is a work of fiction. Any names or characters, businesses or places, events or incidents, are fictitious.\nAny resemblance to actual persons, living or dead, or actual events, is purely coincidental.\n")
+print("No animals were harmed during the making of this program.\n")
